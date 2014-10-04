a2ecf8b06a55a6af643015a47359ff260a4b74d4
Varun Bhardwaj <vforvarun3@gmail.com>
2014-10-04 18:03:40 -0400

    add 1_18

---
+++ 1_18.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define MAXLINE 1000 /* maximum input line size */
+
+int getLine(char line[], int maxline);
+int trim(char input[], char[] output);
+
+int main () {
+	int len;
+	char line[MAXLINE];
+	char trimmed_line[MAXLINE];
+	
+	while ( (len = getLine(line, MAXLINE)) > 0 ) {
+		
+	}
+	return 0;
+}
+
+int getLine (char s[], int lim) {
+	int c, i;
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) 
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+int trim(char input[], char[] output) {
+
+}
+
